6e2c4f1cbe767ae8fed25c54994ca0853e1f9b5f
kkobayashi <suitsitekert@gmail.com>
2024-08-15 17:26:02 +0900

    Initial Commit

---
+++ knowingidol/knowingidolApp.swift
@@ -0,0 +1,17 @@
+//
+//  knowingidolApp.swift
+//  knowingidol
+//
+//  Created by 小林弘平 on 2024/08/15.
+//
+
+import SwiftUI
+
+@main
+struct knowingidolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
